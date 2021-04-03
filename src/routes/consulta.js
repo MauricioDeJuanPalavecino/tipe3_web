ab804eb7a98f0a2359cfda2e2eccec45761ae814
Mauricio Eduardo De Juan Palavecino <63929790+MauricioDeJuanPalavecino@users.noreply.github.com>
2021-04-03 18:09:44 -0300

    modifique consulta para aceptar y rechazar solis

--- src/routes/consulta.js
+++ src/routes/consulta.js
@@ -12,4 +12,41 @@ router.get('/', (req,res) =>{
     res.render('index.ejs')
 });
 
+router.get('/admin', (req,res) =>{
+    conn.query('Select * from emprendedor', (err,resp,campos) => {
+        //console.log(resp);
+        res.render('administrador.ejs',   { datos: resp });
+    });
+});
+
+router.post('/modificar/:nombre_usuario', (req,res,err) =>{
+   
+    
+    conn.query('UPDATE emprendedor SET activo=1 WHERE nombre_usuario = ?', [req.params.nombre_usuario], (err, resp, campos) => {
+        if(!err){
+            console.log("emprendedor aceptado")
+            res.redirect('/admin')
+        }else{
+            console.log(err);
+        }
+    });
+});
+
+router.get('/eliminar/:nombre_usuario', (req,res,err) =>{
+   
+
+    const { nombre_usuario } = req.params;
+    console.log(req.params)
+    conn.query('DELETE from emprendedor WHERE nombre_usuario = ?', [nombre_usuario], (err, resp, campos) => {
+        if(!err){
+            console.log("emprendedor rechazado")
+            res.redirect('/admin')
+        }else{
+            console.log(err);
+        }
+    });
+});
+
+
+
 module.exports = router;
