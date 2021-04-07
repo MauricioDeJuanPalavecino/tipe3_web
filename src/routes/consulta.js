4c4014e197d3dd5efc40d938f13c835b8147233a
Mauricio Eduardo De Juan Palavecino <63929790+MauricioDeJuanPalavecino@users.noreply.github.com>
2021-04-06 21:21:09 -0400

    cambios

--- src/routes/consulta.js
+++ src/routes/consulta.js
@@ -23,6 +23,23 @@ router.get('/', (req,res) =>{
 //     });
 // });
 
+router.post('/FormularioProducto',(req, res) => {
+    const {nombre_usuario,precio, nombre_producto, imagen, descripcion_produc} = req.body;
+    conn.query('INSERT into producto SET? ',{
+        nombre_usuario: nombre_usuario,
+        precio: precio,
+        nombre_producto: nombre_producto,
+        imagen: req.files[0].filename,
+        descripcion_produc: descripcion_produc
+    }, (err, result) => {
+        if(!err) {
+            res.redirect('/consulta');
+        } else {
+            console.log(err);
+        }
+    });
+});
+
 router.get('/admin', (req,res,next) =>{
 
     if(req.isAuthenticated()) return next();
@@ -32,10 +49,13 @@ router.get('/admin', (req,res,next) =>{
     },(req,res,err) =>{
    
     conn.query('Select * from emprendedor', (err,resp,campos) => {
-        //console.log(resp);
-        res.render('administrador.ejs',   { datos: resp });
+        conn.query('Select * from producto', (err,resp1,campos) => {
+            res.render('administrador.ejs',   { datos: resp ,datos1: resp1});
+        });
     });
 });
+
+
 
 router.post('/modificar/:nombre_usuario', (req,res,err) =>{
    
@@ -43,6 +63,18 @@ router.post('/modificar/:nombre_usuario', (req,res,err) =>{
     conn.query('UPDATE emprendedor SET activo=1 WHERE nombre_usuario = ?', [req.params.nombre_usuario], (err, resp, campos) => {
         if(!err){
             console.log("emprendedor aceptado")
+            res.redirect('/admin')
+        }else{
+            console.log(err);
+        }
+    });
+});
+
+router.post('/modificar1/:id_producto', (req,res,err) =>{
+   
+    conn.query('UPDATE producto SET activo=1 WHERE id_producto = ?', [req.params.id_producto], (err, resp, campos) => {
+        if(!err){
+            console.log("producto aceptado")
             res.redirect('/admin')
         }else{
             console.log(err);
@@ -58,6 +90,21 @@ router.get('/eliminar/:nombre_usuario', (req,res,err) =>{
     conn.query('DELETE from emprendedor WHERE nombre_usuario = ?', [nombre_usuario], (err, resp, campos) => {
         if(!err){
             console.log("emprendedor rechazado")
+            res.redirect('/admin')
+        }else{
+            console.log(err);
+        }
+    });
+});
+
+router.get('/eliminar1/:id_producto', (req,res,err) =>{
+   
+
+    const { id_producto } = req.params;
+    console.log(req.params)
+    conn.query('DELETE from producto WHERE id_producto = ?', [id_producto], (err, resp, campos) => {
+        if(!err){
+            console.log("producto rechazado")
             res.redirect('/admin')
         }else{
             console.log(err);
